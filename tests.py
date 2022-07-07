4f3f538983912f837e494aac0ade630aebebccd2
matthewjrogers <matthew.rogers09@gmail.com>
2022-07-07 15:12:22 -0400

    add test for double run of 4

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ Created on Thu Jul  7 14:11:59 2022
 
 @author: mattr
 """
-if __name__ == 'main':
+if __name__ == '__main__':
     from deck_and_card_objects import Deck
     from collections import deque
     from functions import run_check
@@ -22,15 +22,17 @@ if __name__ == 'main':
     r4 = make_hand([0, 1, 2, 3, 12])
     r5 = make_hand(range(5))
     doublerun3 = make_hand([0, 1, 2, 11, 13])
+    doublerun4 = make_hand([0, 1, 2, 3, 13])
     trip_run = make_hand([0, 1, 2, 13, 26])
     quad_run = make_hand([0, 1, 2, 13, 14])
     
-    assert run_check(r0, None) == 0, "No runs"
-    assert run_check(r3, None) == 3, "One run, length 3"
-    assert run_check(r4, None) == 4, "One run, length 4"
-    assert run_check(r5, None) == 4, "One run, length 5"
-    assert run_check(doublerun3, None) == 6, "2 runs, length 3"
-    assert run_check(trip_run, None) == 9, "3 runs, length 3"
-    assert run_check(quad_run, None) == 12, "4 runs, length 3"
+    assert run_check(r0, None) == 0, "Error in no runs"
+    assert run_check(r3, None) == 3, "Error in 1 run, length 3"
+    assert run_check(r4, None) == 4, "Error in 1 run, length 4"
+    assert run_check(r5, None) == 5, "Error in 1 run, length 5"
+    assert run_check(doublerun3, None) == 6, "Error in 2 runs, length 3"
+    assert run_check(doublerun4, None) == 8, "Error in 2 runs, length 4"
+    assert run_check(trip_run, None) == 9, "Error in 3 runs, length 3"
+    assert run_check(quad_run, None) == 12, "Error in 4 runs, length 3"
 
     print("All Checks Passed")
